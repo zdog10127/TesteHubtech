1707eacf6e21e3e9d1b2b77371c97e1cfdf2e982
gabriel teles <gabrieltelessantos48@gmail.com>
2021-03-30 14:36:45 -0300

    Criação das Entidades

---
+++ Hubtech.Dominio/Entidades/Pedido.cs
@@ -0,0 +1,31 @@
+﻿using Hubtech.Dominio.ObjetoDeValor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hubtech.Dominio.Entidades
+{
+    public class Pedido
+    {
+        public int Id { get; set; }
+        public DateTime DataPedido {get;set;}
+        public int UsuarioId { get; set; }
+        public DateTime DataPrevistaEntrega { get; set; }
+        public string CEP { get; set; }
+        public string Estado { get; set; }
+        public string Cidade { get; set; }
+        public string EnderecoCompleto { get; set; }
+        public int NumeroEndereco { get; set; }
+        public int FormaPagamentoId { get; set; }
+        public FormaPagamento FormaPagamento { get; set; }
+
+        /// <summary>
+        /// Pedido deve ter pelo menos um pedido 
+        /// ou muitos pedidos
+        /// </summary>
+
+        public ICollection<ItemPedido> ItensPedido { get; set; }
+    }
+}
